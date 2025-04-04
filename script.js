3c326af44c9d038d052a6905ab7eced15a37357d
Yash Raghubanshi <yashraghubanshi@Yashs-MacBook-Pro-3.local>
2025-04-04 20:12:56 +0530

    nothing

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const aside = document.querySelector('aside');                 
     const backdrop = document.querySelector('.backdrop');         
     const closeButton = document.querySelector('aside button.close'); 
-    const joinTeslaButton = document.querySelector('.car .button');  
+    const joinTeslaButton = document.querySelector('.car .button'); 
 
     menuButton.addEventListener('click', function (event) {
         event.preventDefault();
